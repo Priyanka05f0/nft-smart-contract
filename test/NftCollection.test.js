3b5ce74ffe518376474e96c2d905b1eac8a76e87
Priyanka05f0 <lakshmipriyankabethampudi.23@gmail.com>
2026-01-17 14:41:50 +0530

    Add full ERC721 test coverage

--- test/NftCollection.test.js
+++ test/NftCollection.test.js
@@ -13,9 +13,13 @@ describe("NftCollection", function () {
     await nft.waitForDeployment();
   });
 
+  /* ---------------- DEPLOYMENT ---------------- */
+
   it("should deploy successfully", async function () {
     expect(await nft.getAddress()).to.properAddress;
   });
+
+  /* ---------------- PAUSE / UNPAUSE ---------------- */
 
   it("admin should be able to pause minting", async function () {
     await nft.connect(owner).pauseMinting();
@@ -31,6 +35,50 @@ describe("NftCollection", function () {
       nft.connect(user).pauseMinting()
     ).to.be.revertedWith("Not admin");
   });
+
+  /* ---------------- MINTING ---------------- */
+
+  it("should allow admin to mint NFT", async function () {
+    await nft.connect(owner).mint(owner.address);
+    expect(await nft.ownerOf(0)).to.equal(owner.address);
+  });
+
+  it("should emit Transfer event on mint", async function () {
+    await expect(nft.connect(owner).mint(owner.address))
+      .to.emit(nft, "Transfer")
+      .withArgs(ethers.ZeroAddress, owner.address, 0);
+  });
+
+  it("should revert minting to zero address", async function () {
+    await expect(
+      nft.connect(owner).mint(ethers.ZeroAddress)
+    ).to.be.reverted;
+  });
+
+  /* ---------------- TRANSFERS ---------------- */
+
+  it("should transfer NFT between users", async function () {
+    await nft.connect(owner).mint(owner.address);
+    await nft.connect(owner).transferFrom(owner.address, user.address, 0);
+    expect(await nft.ownerOf(0)).to.equal(user.address);
+  });
+
+  /* ---------------- APPROVALS ---------------- */
+
+  it("should approve another address", async function () {
+    await nft.connect(owner).mint(owner.address);
+    await nft.connect(owner).approve(user.address, 0);
+    expect(await nft.getApproved(0)).to.equal(user.address);
+  });
+
+  it("should set operator approval", async function () {
+    await nft.connect(owner).setApprovalForAll(user.address, true);
+    expect(
+      await nft.isApprovedForAll(owner.address, user.address)
+    ).to.equal(true);
+  });
+
+  /* ---------------- INVALID TOKEN ---------------- */
 
   it("tokenURI should revert for invalid tokenId", async function () {
     await expect(nft.tokenURI(0)).to.be.revertedWith("Token does not exist");
